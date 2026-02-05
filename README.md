b2c7c8eae5ac166cfcb2bbb8990e80716012b0ef
hector-munachi <victornothector@gmail.com>
2026-02-05 02:44:34 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# Invoice App
+
+A web-based invoice generator application built with Next.js 13, TypeScript, React, and the Shadcn UI library. It provides an easy way to create and manage professional invoices.
+
+## Technologies
+
+### Core Technologies
+
+- **Next.js:** React framework for SSR and client-side navigation.
+- **TypeScript:** JavaScript superset with static typing.
+- **Shadcn-UI:** UI library for enhanced visuals.
+- **Tailwind:** Utility-first CSS framework.
+- **React Hook Form:** Form management for React.
+- **Zod:** TypeScript-first schema validation.
+- **Puppeteer:** PDF generation with headless browsers.
+
+### Additional Dependencies
+
+- **Nodemailer:** Node.js module for sending emails.
+- **Lucide Icons:** Collection of customizable SVG icons.
+
+## Getting Started
+
+Follow these instructions to get Invoice App up and running on your local machine.
+
+### Prerequisites
+
+- Node.js and npm installed on your system.
+
+### Installation
+
+1. Clone the repository:
+
+    ```bash
+    git clone https://github.com/
+    cd invoice-app
+    ```
+
+2. Install dependencies
+
+    ```bash
+    npm install
+    ```
+
+3. Create an .env.local file with this content (This step is for sending pdf to email feature):
+    ```env
+    NODEMAILER_EMAIL=your_email@example.com
+    NODEMAILER_PW=your_email_password
+    ```
+4. Start development server
+
+    ```bash
+    npm run dev
+    ```
+
+5. Open your web browser and access the application at [http://localhost:3000](http://localhost:3000)
+ <!-- LICENSE -->
+
+## License
+
+Distributed under the MIT License.
---
+++ app/[locale]/layout.tsx
@@ -0,0 +1,109 @@
+// Components
+import { BaseFooter, BaseNavbar } from "@/app/components";
+// ShadCn
+import { Toaster } from "@/components/ui/toaster";
+// Contexts
+import Providers from "@/contexts/Providers";
+// Fonts
+import {
+    alexBrush,
+    dancingScript,
+    greatVibes,
+    outfit,
+    parisienne,
+} from "@/lib/fonts";
+// SEO
+import { JSONLD, ROOTKEYWORDS } from "@/lib/seo";
+// Variables
+import { BASE_URL, GOOGLE_SC_VERIFICATION, LOCALES } from "@/lib/variables";
+
+// Vercel Analytics
+import { Analytics } from "@vercel/analytics/react";
+import type { Metadata } from "next";
+// Next Intl
+import { NextIntlClientProvider } from "next-intl";
+import { notFound } from "next/navigation";
+
+export const metadata: Metadata = {
+    title: "Invoice App | Invoice Generator",
+    description:
+        "Create invoices effortlessly, the free invoice generator. Try it now!",
+    icons: [{ rel: "icon", url: "/favicon.ico" }],
+    keywords: ROOTKEYWORDS,
+    robots: {
+        index: true,
+        follow: true,
+    },
+    alternates: {
+        canonical: BASE_URL,
+    },
+    authors: {
+        name: "Hector Munachi",
+        url: "",
+    },
+    verification: {
+        google: GOOGLE_SC_VERIFICATION,
+    },
+};
+
+export const viewport = {
+    width: "device-width",
+    initialScale: 1,
+};
+
+export function generateStaticParams() {
+    // Next.js expects an array of objects: [{ locale: 'en' },
+    // ...]
+    const locales = LOCALES.map((locale) => ({ locale: locale.code }));
+    return locales;
+}
+
+export default async function LocaleLayout(props: {
+    children: React.ReactNode;
+    params: Promise<{ locale: string }>;
+}) {
+    const params = await props.params;
+
+    const { locale } = params;
+
+    const { children } = props;
+
+    let messages;
+    try {
+        messages = (await import(`@/i18n/locales/${locale}.json`)).default;
+    } catch (error) {
+        notFound();
+    }
+
+    return (
+        <html lang={locale} suppressHydrationWarning>
+            <head suppressHydrationWarning>
+                <script
+                    type="application/ld+json"
+                    id="json-ld"
+                    dangerouslySetInnerHTML={{ __html: JSON.stringify(JSONLD) }}
+                />
+            </head>
+            <body
+                className={`${outfit.className} ${dancingScript.variable} ${parisienne.variable} ${greatVibes.variable} ${alexBrush.variable} antialiased bg-slate-100 dark:bg-slate-800`}
+                suppressHydrationWarning
+            >
+                <NextIntlClientProvider locale={locale} messages={messages}>
+                    <Providers>
+                        <BaseNavbar />
+
+                        <div className="flex flex-col">{children}</div>
+
+                        <BaseFooter />
+
+                        {/* Toast component */}
+                        <Toaster />
+
+                        {/* Vercel analytics */}
+                        <Analytics />
+                    </Providers>
+                </NextIntlClientProvider>
+            </body>
+        </html>
+    );
+}
